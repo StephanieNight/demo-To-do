22c77da2fee0ecf8ec76a9bae9f8f0ff8ea9ade5
Stephanie Night <s.hvenegaard@outlook.com>
2022-08-27 15:57:57 +0200

    updated to use storage libery. not the best solution but it works now

--- Todo-App/Startup.cs
+++ Todo-App/Startup.cs
@@ -12,6 +12,7 @@ using Todo_App.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Storage;
 
 namespace Todo_App
 {
@@ -27,11 +28,11 @@ namespace Todo_App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<TodoContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<TodoContext>();
             services.AddControllersWithViews();
            services.AddRazorPages();
         }

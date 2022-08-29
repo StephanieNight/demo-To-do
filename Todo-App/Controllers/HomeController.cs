4429fd93c60f469ec4d5cb6aabf662ff967dc714
Stephanie Night <s.hvenegaard@outlook.com>
2022-08-29 22:19:06 +0200

    implimented base controller

--- Todo-App/Controllers/HomeController.cs
+++ Todo-App/Controllers/HomeController.cs
@@ -1,37 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Storage.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Todo_App.Models;
-using static System.Net.WebRequestMethods;
 
 namespace Todo_App.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        static HttpClient httpClient = new HttpClient();
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger) : base(logger)
         {
-            _logger = logger;
         }
 
         public async Task<IActionResult> Index()
         {
-            //string requestUrl = _config.GetSection("AppSettings").GetSection("APIUrl").Value;
-            //string requestKey = _config.GetSection("AppSettings").GetSection("Key").Value;
-
-            string requestUrl = "https://nights-todolist-api.azurewebsites.net/api/5eeace50-5886-421a-95a2-753bb34fa340/lists";
-            string requestKey = "orlFiRCWi0RbyRWp5efxc7SdFBY8z6OckeLylyp2EusrAzFubSStrA==";
-
             try
             {
                 httpClient.DefaultRequestHeaders.Add("x-functions-key", requestKey);
@@ -40,8 +27,9 @@ namespace Todo_App.Controllers
                 IEnumerable<TodoList> response = JsonConvert.DeserializeObject<IEnumerable<TodoList>>(content);
                 return View(response);
             }
-            catch (Exception)
+            catch (Exception e)
             {
+                _logger.LogError(e.ToString());
                 return View(new List<TodoList>());
             }
         }
@@ -56,5 +44,6 @@ namespace Todo_App.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
